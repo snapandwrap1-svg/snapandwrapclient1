dc61f0843f94ee942e9d6540725d79cfb316e8f7
snapandwrap1-svg <snapandwrap1@gmail.com>
2026-06-15 20:52:37 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey AIzaSyA5G0v1vV7O1z-868Z2X_J2Y4m0W2k9e0,
+  authDomain magic-video-frame-aee7b.firebaseapp.com,
+  databaseURL httpsmagic-video-frame-aee7b-default-rtdb.firebaseio.com,
+  projectId magic-video-frame-aee7b,
+  storageBucket magic-video-frame-aee7b.appspot.com,
+  messagingSenderId 305374824892,
+  appId 1305374824892webcbd5db50b70c3ba393a525
+};
+
+const IMGBB_API_KEY = 230e8bef11eb674fee79f237dac039e9;
+const CLOUDINARY_CLOUD = dlvepuk9i;
+const CLOUDINARY_PRESET = ml_default;
